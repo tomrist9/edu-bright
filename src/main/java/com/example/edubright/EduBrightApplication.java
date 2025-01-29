f45e2e6e85d51d96d436567907cf59c3098efc74
Tomris Teymurlu <tomristt9@gmail.com>
2025-01-29 19:10:58 +0400

    initial commit

---
+++ src/main/java/com/example/edubright/EduBrightApplication.java
@@ -0,0 +1,13 @@
+package com.example.edubright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduBrightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduBrightApplication.class, args);
+    }
+
+}
